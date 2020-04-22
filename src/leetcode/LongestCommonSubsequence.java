6675cc0b52e64291afba22ddb9b9c08a85eaec29
shiyan <shiyan9128@gmail.com>
2020-04-22 16:38:22 +0800

    s[i] != t[j] 时，dp[i][j]取dp[i+1][j], dp[i][j+1]
    dp[i+1][j]， dp[i][j+1] 一定大于 dp[i+1][j+1]，不需要再比较

--- src/leetcode/LongestCommonSubsequence.java
+++ src/leetcode/LongestCommonSubsequence.java
@@ -13,7 +13,7 @@ class Solution {
                 if (source[i] == target[j]) {
                     dp[i][j] = dp[i+1][j+1] + 1;
                 } else {
-                    dp[i][j] = Math.max(Math.max(dp[i+1][j+1], dp[i][j+1]), dp[i+1][j]);
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j+1]);
                 }
             }
         }
